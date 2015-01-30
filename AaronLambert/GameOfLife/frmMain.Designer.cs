0699a987f3ef452b04a953b225efd7370dc3a103
StyroCupMan <styrocupman@gmail.com>
2015-01-30 08:15:20 -0700

    Solution

---
+++ AaronLambert/GameOfLife/frmMain.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace GameOfLife
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlWorld = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.LifeTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnlWorld
+            // 
+            this.pnlWorld.BackColor = System.Drawing.Color.White;
+            this.pnlWorld.Location = new System.Drawing.Point(13, 13);
+            this.pnlWorld.Name = "pnlWorld";
+            this.pnlWorld.Size = new System.Drawing.Size(451, 451);
+            this.pnlWorld.TabIndex = 0;
+            this.pnlWorld.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlWorld_Paint);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(198, 470);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 26);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // LifeTimer
+            // 
+            this.LifeTimer.Interval = 5000;
+            this.LifeTimer.Tick += new System.EventHandler(this.LifeTimer_Tick);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Enabled = false;
+            this.btnPlay.Image = global::GameOfLife.Properties.Resources.Play;
+            this.btnPlay.Location = new System.Drawing.Point(359, 470);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(23, 23);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Image = global::GameOfLife.Properties.Resources.Last;
+            this.btnNext.Location = new System.Drawing.Point(388, 470);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(23, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Image = global::GameOfLife.Properties.Resources.Pause;
+            this.btnPause.Location = new System.Drawing.Point(359, 470);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(23, 23);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Visible = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 504);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlWorld);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Game of Life";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlWorld;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer LifeTimer;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPause;
+    }
+}
+
